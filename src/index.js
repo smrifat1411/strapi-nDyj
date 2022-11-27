01b91614255ca58b132412b894b5c830a44036c0
smrifat1411 <smrifat1411@gmmail.com>
2022-11-27 18:54:46 +0600

    configure the admin panel

--- src/index.js
+++ src/index.js
@@ -1,20 +1,44 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.plugin("graphql").service("extension");
 
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/*{ strapi }*/) {},
+    const extension = () => ({
+      typeDefs: `
+        type Query {
+          post(slug: String!): PostEntityResponse,
+          
+        }
+      `,
+      resolvers: {
+        Query: {
+          post: {
+            resolve: async (parent, args, context) => {
+              const { toEntityResponse } = strapi.service(
+                "plugin::graphql.format"
+              ).returnTypes;
+              const data = await strapi.services["api::post.post"].find({
+                filters: { slug: args.slug },
+              });
+              const response = toEntityResponse(data.results[0]);
+              return response;
+            },
+          },
+        },
+      },
+    });
+    extensionService.use(extension);
+  },
+  async bootstrap({ strapi }) {
+    // for(let i =0;i <100;i++){
+    //   strapi.entityService.create("api::photo.photo",{
+    //     data:{
+    //       caption:" Hello",
+    //       content:faker.lorem.paragraph(),
+    //       image:faker.image.animals(),
+    //     }
+    //   })
+    // }
+  },
 };
